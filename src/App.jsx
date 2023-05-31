db1570cb1765b48e14f6592e73642b8ce4781e30
Seda Icel <sedaozturkicel@gmail.com>
2023-05-31 19:23:32 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Start from "./components/Start"
+import Quiz from "./components/Quiz"
+
+export default function App(){
+//two screen, one is start section, the other is quiz section
+//default : start 
+
+    const [game, setGame] = React.useState(false)
+
+    function startGame(){
+        setGame(true)
+    }
+    return(
+        <main>
+
+            
+            {game ? <Quiz/> : <Start startGame={startGame}/>}
+        </main>
+
+    )
+}
