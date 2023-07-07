8a6dd0af332fd01cbda9c65b30162103d220e245
Seda Icel <sedaozturkicel@gmail.com>
2023-07-07 19:13:44 +0300

    minor fix in Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -43,7 +43,7 @@ export default function Quiz(){
                      <>
                         
                         <input
-                        name="options"
+                        name={question}
                         type="radio" 
                         className="radio-input"
                         id={answer}
