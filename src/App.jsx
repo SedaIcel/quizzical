f002df218b9f47fa3249ff555bbcb090d9613f4b
Seda Icel <sedaozturkicel@gmail.com>
2023-11-22 18:36:03 +0300

    changed styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import React from "react"
-import Start from "./components/Start"
-import Quiz from "./components/Quiz"
+import React from "react";
+import Start from "./components/Start";
+import Quiz from "./components/Quiz";
+import blobs1 from "./assets/blobs1.png";
+import blobs2 from "./assets/blobs2.png";
 
-export default function App(){
-//two screen, one is start section, the other is quiz section
-//default : start 
+export default function App() {
+  //two screen, one is start section, the other is quiz section
+  //default : start
 
-    const [game, setGame] = React.useState(false)
+  const [game, setGame] = React.useState(false);
 
-    function startGame(){
-        setGame(true)
-    }
-    
+  function startGame() {
+    setGame(true);
+  }
 
-
-
-    return(
-        <main>            
-            {game ? <Quiz/> : <Start startGame={startGame}/>}
-        </main>
-
-    )
+  return (
+    <div className="app">
+      <img src={blobs1} className="appImgLeft" />
+      <img src={blobs2} className="appImgRight" />
+      {game ? <Quiz /> : <Start startGame={startGame} />}
+    </div>
+  );
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,129 +1,71 @@
-import React from "react"
-import {nanoid} from "nanoid"
+import { useState, useEffect } from "react";
+// import { nanoid } from "nanoid";
 
+export default function Quiz() {
+  const [triviaData, setTriviaData] = useState([]);
 
-export default function Quiz(){
-    const [triviaData, setTriviaData] = React.useState([])
-    const [selectedData, setSelectedData] = React.useState([])
-    const [isSelected, setIsSelected] = React.useState(false)
-
-    
-
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-      }
-
-    function insertRandomly(array, element) {
-        const newArray = [...array]; 
-        const index = Math.floor(Math.random() * (newArray.length + 1));
-        newArray.splice(index, 0, element);
-        return newArray;
-      }
-
-    React.useEffect(()=> {
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-            .then(res => res.json())
-            .then(data => setTriviaData(data.results))
-    },[])
-           
-    const newData = triviaData.map((item)=> {
-        return {
-            ...item,
-            id: nanoid(),
-            triviaAnswers : shuffleArray(insertRandomly(item.incorrect_answers,item.correct_answer))
-        }
-    }) 
-
-    //console.log(newData)
-
-
-    function handleClick(event) {
-        setSelectedData(newData.map(item => {
-            return item.question === event.target.name ?
-            {
-                ...item,
-                selectedAnswer : event.target.value
-            } :
-            {
-                ...item,            
-                selectedAnswer : ""
-            }
-        }))        
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
+  }
 
-    console.log(selectedData)
-  
-    const result = selectedData.filter(item => item.correct_answer === item.selectedAnswer).length
-    
-    // function checkAnswers() {
-    //     const 
-    //    selectedData.map(item => {
-    //     if(item.selectedAnswer) {
+  function insertRandomly(array, element) {
+    const newArray = [...array];
+    const index = Math.floor(Math.random() * (newArray.length + 1));
+    newArray.splice(index, 0, element);
+    return newArray;
+  }
 
-    //     }
-    //    })
-    // }
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+      .then((res) => res.json())
+      .then((data) => setTriviaData(organizeData(data.results)));
+  }, []);
 
-    
-     
-    
+  function organizeData(data) {
+    const newData = data.map((item, index) => {
+      return {
+        key: index,
+        id: index,
+        question: item.question,
+        allAnswers: shuffleArray(
+          insertRandomly(item.incorrect_answers, item.correct_answer)
+        ),
+        correctAnswer: item.correct_answer,
+      };
+    });
+    return newData;
+  }
 
-    const triviaSet = newData.map (({question, triviaAnswers}) => {        
-        return (
-            <>                
-                <h1 className = "container-questions">{question}</h1>
-                <div className = "container-button"
-                                        
-                    >
-                     {triviaAnswers.map(answer =>                      
-                     <>                        
-                        <input                         
-                        name={question}
-                        type="radio" 
-                        className="radio-input"
-                        id={answer}
-                        value={answer}
-                        onChange={handleClick}                        
-                        
-                        >
-                        </input> 
-                        <label htmlFor={answer} className="radio-label">{answer}</label>                    
-                     
-                     </>
-                        )
-                    }
-                </div>   
-                <hr/>                 
+  const newEl = triviaData.map(({ question, allAnswers }) => {
+    console.log(question);
+    console.log(allAnswers);
+
+    return triviaData ? (
+      <div key={question}>
+        <h1 className="container-questions">{question}</h1>
+        <div className="container-button">
+          {allAnswers.map((answer) => (
+            <>
+              <input
+                type="radio"
+                id={answer}
+                name={question}
+                className="radio-label"
+              />
+              <label htmlFor={answer}>{answer}</label>
             </>
-            )
-    })
-
-   
-
-    return(
-        <div className ="quiz">
-            {triviaSet}
-            <div>
-                
-                <p className="quiz-results">You scored {result}/5 correct answers</p>
-                
-                <button 
-                    className="check-answers"
-                    onClick={()=> checkAnswers}
-                >
-                    Check Answers
-                </button>
-
-            </div>
-
+          ))}
         </div>
-       
-    )
+        <hr />
+      </div>
+    ) : (
+      <h1>Loading...</h1>
+    );
+  });
+
+  return <div className="quiz">{newEl}</div>;
 }
-
-
